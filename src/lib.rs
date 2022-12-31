4857b166ef1f6b995b402a1a2ec726f31ab47cb2
fruit-bird <github.ru3w9@slmail.me>
2022-12-31 18:43:45 +0100

    file feature is stable

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,21 @@
 // (pain) convert String to &str in the end
 // (good) make this CLI friendly
 //      x if command takes no args          --> clipboard copy
-//       if commands takes arg (string)    --> output to terminal
+//      + if commands takes arg (string)    --> output to terminal
 //      + remove need for env var
 //      + takes --paste flag to copy the output to the clipboard
 // (have error messages be more expressive and put them in an error enum)
 
 use anyhow::Result;
 use arboard::Clipboard;
-use clap::{arg, command, ArgAction, ArgMatches};
+use clap::{arg, command, ArgMatches};
 use rand::Rng;
-use std::{fmt::Display, fs, path::Path};
+use std::{
+    fmt::Display,
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -29,7 +34,7 @@ pub enum MockerError {
     FileReading,
 }
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct Mocker(String);
 
 impl Mocker {
@@ -49,10 +54,19 @@ impl Mocker {
         Ok(Self(output))
     }
 
-    /// Converts file contents into MoCkiNg case
+    /// Converts file contents into MoCkiNg case, and copies the conversion into a new file
     pub fn new_from_file(path: impl AsRef<Path>) -> Result<Self> {
-        let input = fs::read_to_string(path)?;
+        let input = fs::read_to_string(&path)?;
         let output = Self::mocking_spongebob_case(input);
+        let new_path = path
+            .as_ref()
+            .to_str()
+            .unwrap()
+            .split('.')
+            .take(1)
+            .collect::<String>()
+            + "_mock.md";
+        File::create(new_path)?.write_all(output.as_bytes())?;
         Ok(Self(output))
     }
 
@@ -82,39 +96,38 @@ impl Display for Mocker {
     }
 }
 
-fn cli_builder() -> ArgMatches {
+fn cli() -> ArgMatches {
     let matches = command!()
         .about("Converts text into MoCkiNg case")
-        .arg(arg!([STRING] "Text to convert"))
-        .arg(arg!(-c --clipboard "Converts text from the clipboard"))
+        .arg(arg!([STRING] "Text to convert").conflicts_with("clipboard"))
+        .arg(arg!(-c --clipboard "Converts text from the clipboard").conflicts_with("file"))
         .arg(arg!(-p --paste "Pastes the conversion to the clipboard"))
-        .arg(arg!(-f --file <PATH> "Converts text from a file").action(ArgAction::SetTrue))
+        .arg(arg!(-f --file <path> "Converts text from a file into a new file"))
         .get_matches();
     matches
 }
 
 pub fn run() -> Result<()> {
-    let matches = cli_builder();
+    let matches = cli();
 
     let output = if matches.get_flag("clipboard") {
         Mocker::new_from_clipboard()?
     }
-    // file doesn't work start
-    else if matches.get_flag("file") {
-        match matches.get_one::<String>("PATH") {
+    // get_flag doesn't work here, but contains_id does for some reason
+    // will do for now
+    else if matches.contains_id("file") {
+        match matches.get_one::<String>("file") {
             Some(path) => Mocker::new_from_file(path)?,
             None => return Err(MockerError::FileReading.into()),
         }
-    }
-    // file doesn't work end
-    else {
+    } else {
         match matches.get_one::<String>("STRING") {
             Some(text) => Mocker::new(text)?,
             None => return Err(MockerError::NoInput.into()),
         }
     };
 
-    println!("\n{}\n", output.0);
+    println!("\n{}\n", output);
 
     if matches.get_flag("paste") {
         Clipboard::new()?.set_text(output.0)?;
